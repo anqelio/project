5a2f7aec63481675a5c57d0a8edbfbe2683596dc
anqelio <167873684+anqelio@users.noreply.github.com>
2024-09-12 00:00:58 +0500

    Delete project_third.py

--- project_third.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime, timedelta
-try:
-   if (brave == True) or  (leader and favorite_subject == ('Защита от тёмных исскуств')):
-    print('Студент зачислен в Гриффиндор')
-except ValueError as e:
-    print('Некорректный ввод даты')
