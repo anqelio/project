4f32881417273a9d02a58666d5b3940c4b4f5fb2
rotoyouoi <rotoyouoi@yandex.ru>
2024-09-11 10:57:14 +0500

    реализована первая задача

---
+++ project_third.py
@@ -0,0 +1,6 @@
+from datetime import datetime, timedelta
+try:
+   if (brave == True) or  (leader and favorite_subject == ('Защита от тёмных исскуств')):
+    print('Студент зачислен в Гриффиндор')
+except ValueError as e:
+    print('Некорректный ввод даты')
